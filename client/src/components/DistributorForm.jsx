8bed8efa9e9c51e69c771a24a6d58d7058470a22
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-11-25 02:52:16 +0700

    feat: add data country for droplist

--- client/src/components/DistributorForm.jsx
+++ client/src/components/DistributorForm.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
+import { countries } from "../data/countries";
 
 export default function DistributorForm({ handleSubmit, propName, dataDistributor }) {
     const navigate = useNavigate();
@@ -57,12 +58,19 @@ export default function DistributorForm({ handleSubmit, propName, dataDistributo
 
             <div className="mb-4">
                 <label className="block text-sm font-medium mb-1">Country</label>
-                <input 
-                    type="text"
+                <select 
                     value={country}
                     onChange={(e) => setCountry(e.target.value)}
+                    required
                     className="w-full border border-gray-300 rounded px-3 py-2"
-                />
+                >
+                    <option value="">-- Select Country --</option>
+                    {countries.map((countryName) => (
+                        <option key={countryName} value={countryName}>
+                            {countryName}
+                        </option>
+                    ))}
+                </select>
             </div>
 
             <div className="mb-4">
